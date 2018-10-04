6b8c859bbd8bb4424b7ce724eb5cc7993ac13cdb
Andrey Eremeev <a.eremeev@corp.mail.ru>
2018-10-04 18:59:12 +0300

    link to ln -s

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
-link .vimrc ~/.vimrc
+ln -s .vimrc ~/.vimrc
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-link .tmux.conf ~/.tmux.conf
-link .zshrc ~/.zshrc
+ln -s .tmux.conf ~/.tmux.conf
+ln -s .zshrc ~/.zshrc
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
