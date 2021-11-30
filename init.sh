5f0b3103bd8d5363583625ccf0774acee7065bfe
Andrey Eremeev <a.eremeev@corp.mail.ru>
2021-11-30 15:11:18 +0300

    add nvim redirector

--- init.sh
+++ init.sh
@@ -2,3 +2,4 @@ ln -s $PWD/.vimrc ~/.vimrc
 ln -s $PWD/.tmux.conf ~/.tmux.conf
 ln -s $PWD/.zshrc ~/.zshrc
 ln -s $PWD/git_diff_wrapper ~/.git_diff_wrapper
+ln -s $PWD/init.vim ~/.config/nvim/init.vim
