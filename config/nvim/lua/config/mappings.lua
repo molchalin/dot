a9e79e4db3d843c61f1da31f3f163db19c162af1
Andrei Eremeev <Axtone4all@yandex.ru>
2023-03-26 23:44:07 +0200

    nvim: add some mappings

--- config/nvim/lua/config/mappings.lua
+++ config/nvim/lua/config/mappings.lua
@@ -3,3 +3,15 @@ vim.g.mapleader = ","
 vim.keymap.set("n", "<leader>e",  ":tabedit<space>",  { remap = false })
 vim.keymap.set("n", "<leader>q",  ":wqa<cr>",         { remap = false })
 vim.keymap.set("n", "*",          "*``",              { remap = false })
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "J", "mzJ`z")
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzz")
+vim.keymap.set("n", "N", "Nzz")
+
+vim.keymap.set("x", "<leader>p", "\"_dP")
