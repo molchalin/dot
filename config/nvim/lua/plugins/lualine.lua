7614365426f1558304a28468caef778cd0d73344
Andrei Eremeev <aeremeev@cinemo.com>
2023-10-05 10:00:01 +0200

    lualine: relative filepath

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -5,6 +5,14 @@ return {
       options = {
         theme = 'gruvbox-material',
       },
+      sections = {
+        lualine_a = {'mode'},
+        lualine_b = {'branch', 'diff', 'diagnostics'},
+        lualine_c = {{'filename', path = 1}},
+        lualine_x = {'encoding', 'fileformat', 'filetype'},
+        lualine_y = {'progress'},
+        lualine_z = {'location'}
+      },
       tabline = {
         lualine_a = {
           {
