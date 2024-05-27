d53a5c7bf12700fa97404bdfc812a8ec1b236cbb
Andrei Eremeev <aeremeev@cinemo.com>
2024-05-27 20:03:33 +0200

    nvim: remove commentary

--- config/nvim/lua/plugins/commentary.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "tpope/vim-commentary",
-  event = "VeryLazy",
-  config = function()
-    vim.keymap.set("n", "<leader>td", "oTODO(a.eremeev) <esc>:Commentary<cr>A", { remap = false })
-  end,
-}
