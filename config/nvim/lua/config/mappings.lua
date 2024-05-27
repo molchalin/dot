eb3e4f9de58647ba4b46258cc42c5fc5f1121fa6
Andrei Eremeev <aeremeev@cinemo.com>
2024-05-27 11:51:49 +0200

    nvim: comment some mappings

--- config/nvim/lua/config/mappings.lua
+++ config/nvim/lua/config/mappings.lua
@@ -2,16 +2,21 @@ vim.g.mapleader = ","
 
 vim.keymap.set("n", "<leader>e",  ":tabedit<space>",  { remap = false })
 vim.keymap.set("n", "<leader>q",  ":wqa<cr>",         { remap = false })
+
+-- don't jump to next match
 vim.keymap.set("n", "*",          "*``",              { remap = false })
 
+-- move blocks of code in visual mode
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("n", "J", "mzJ`z")
 
+-- put cursor in the middle of the screen when jumping
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzz")
 vim.keymap.set("n", "N", "Nzz")
+vim.keymap.set("n", "GG", "GGzz")
 
 vim.keymap.set("x", "<leader>p", "\"_dP")
