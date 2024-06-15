ce69223bc67dcedeb0351193e60495bf8dfe7d3a
Andrei Eremeev <aaeremeev@proton.me>
2024-06-15 12:19:18 +0200

    tmux: add weather

--- config/nvim/lua/plugins/tmuxline.lua
+++ config/nvim/lua/plugins/tmuxline.lua
@@ -5,6 +5,19 @@ return {
   end,
 
   config = function()
+    vim.g.tmuxline_preset = { 
+      a = '#S',
+      -- b = '',
+      -- c = '',
+      win = {'#I', '#W'},
+      cwin = {'#I', '#W'},
+      x = {"#(~/.tmux/plugins/tmux-weather/scripts/weather.sh)"},
+      y = {'%Y-%m-%d', '%H:%M'},
+      z = "#(whoami)",
+      options = {
+        ["status-justify"] = 'left',
+      },
+    }
     vim.cmd[[ :Tmuxline vim_statusline_3 ]]
   end,
 }
