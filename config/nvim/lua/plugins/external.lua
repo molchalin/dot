d7fc6b55e637fa986df07b4b11d469076dc9b301
Andrei Eremeev <aaeremeev@proton.me>
2024-07-16 14:10:07 +0200

    nvim: remove wastebin

--- config/nvim/lua/plugins/external.lua
+++ config/nvim/lua/plugins/external.lua
@@ -1,14 +1,5 @@
 return{
   {
-    "matze/wastebin.nvim",
-    event = "VeryLazy",
-    name = "wastebin",
-    cond = function()
-      return vim.env.WASTEBIN_URL ~= nil
-    end,
-    opts = {},
-  },
-  {
     "iamcco/markdown-preview.nvim",
     event = "VeryLazy",
     ft = "markdown",
