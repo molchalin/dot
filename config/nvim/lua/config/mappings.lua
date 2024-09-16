bbf8bee568c1799ac8ac7b00fcfd7225538ca18a
Andrei Eremeev <aaeremeev@proton.me>
2024-09-16 16:13:02 +0200

    nvim: add todo mapping

--- config/nvim/lua/config/mappings.lua
+++ config/nvim/lua/config/mappings.lua
@@ -40,3 +40,4 @@ noremap("n", "cp", "<cmd>cprev<cr>", { desc = "prev quicklist entry" })
 
 noremap("n", "gco", "o<esc>Vcx<esc><cmd>normal gcc<cr>fxa<bs>", { desc = "add comment below" })
 noremap("n", "gcO", "O<esc>Vcx<esc><cmd>normal gcc<cr>fxa<bs>", { desc = "add comment above" })
+noremap("n", "<leader>td", "<cmd>normal gcOTODO(" .. vim.env.USER .. "): <cr>a", { desc = "todo comment"})
