febf6052f039f4110aa24006e418ca35b9eb734b
Andrei Eremeev <aaeremeev@proton.me>
2025-01-21 10:19:06 +0100

    setup: add gnome

--- setup.sh
+++ setup.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 
+RED='\033[0;31m'
+BLUE='\033[0;34m'
+GRAY='\033[1;30m'
+NC='\033[0m'
+
 function log_info() {
   if [ "$verbose" == true ]; then
-    echo "[I] $@";
+    echo -e "${GRAY}[I] $@${NC}";
   fi
 }
 
 function log_fatal() {
-  echo "[F] $@";
+  echo -e "${RED}[F] $@${NC}";
   exit 1;
 }
 
@@ -16,7 +21,7 @@ function is_mac() {
 }
 
 function execute() {
-  echo "[E] $@"
+  echo -e "${BLUE}[C] $@${NC}"
   if [ "$execute" == true ]; then
     eval $@;
   fi
@@ -53,7 +58,7 @@ function link() {
   elif [[ -e "$link_path" ]]; then
     log_info "some file already exists at $link_path"
   else
-    execute 'ln -s "'$file_path'" "'$link_path'"'
+    execute "ln -s '$file_path' '$link_path'"
   fi
 }
 
@@ -68,7 +73,7 @@ function make_dir() {
   elif [[ -e "$dir_path" ]]; then
     log_info "some file already exists at $dir_path"
   else
-    execute 'mkdir -p "'$dir_path'"'
+    execute "mkdir -p '$dir_path'"
   fi
 }
 
@@ -78,8 +83,47 @@ function get_file() {
   if [[ -e "$path" ]]; then
     log_info "$path already exists"
   else
-    execute 'curl "'$url'" -o "'$path'"'
+    execute "curl '$url' -o '$path'"
   fi
+}
+
+function set_gnome_option() {
+  local schema="$1"
+  local key="$2"
+  local value="$3"
+  local prev_value=$(gsettings get "$schema" "$key")
+  log_info "gsetings set '$schema' '$key': $prev_value -> '$value'"
+  execute "gsettings set '$schema' '$key' '$value'"
+}
+
+function install_emoji_picker() {
+  local exit_code=0
+  gnome-extensions show emoji-copy@felipeftn 2>&1 > /dev/null || exit_code=$?
+  if [[ $exit_code -ne 0 ]]; then
+    install "gnome-shell-extension-emoji-copy"
+  else
+    log_info "emoji-copy is already installed"
+  fi
+  execute "gnome-extensions enable emoji-copy@felipeftn"
+}
+
+function install_font() {
+  local font_name="$1"
+  local font_package="$2"
+  local exit_code=0
+  fc-list "'$font_name'" -q || exit_code=$?
+  if [[ $exit_code -ne 0 ]]; then
+    install "'$font_package'"
+  else
+    log_info "font $1 is already installed"
+  fi
+}
+
+function install_sf_fonts() {
+  # TODO(andrei): Add package for mac os
+  install_font "SF Pro" "otf-san-francisco"
+  # TODO(andrei): Add package for mac os
+  install_font "SF Mono" "otf-san-francisco-mono"
 }
 
 set -o errexit
@@ -91,6 +135,7 @@ function setup_zsh() {
   fi
   link "config/zshrc" ".zshrc"
   link_same_name "config/zsh/" ".config/"
+  # TODO(andrei): change shell for user
 }
 
 function setup_nvim() {
@@ -113,7 +158,23 @@ function setup_tmux() {
   link_same_name "bin/tmux-realpath" ".local/bin/"
 }
 
-components=('zsh' 'nvim' 'tmux')
+function setup_gnome() {
+  install_emoji_picker
+  set_gnome_option org.gnome.desktop.input-sources sources "[('xkb', 'us'), ('xkb', 'ru')]"
+  set_gnome_option org.gnome.desktop.wm.keybindings switch-input-source "['<Primary>space']"
+  set_gnome_option org.gnome.desktop.peripherals.keyboard delay 175
+  set_gnome_option org.gnome.desktop.peripherals.keyboard repeat-interval 25
+  set_gnome_option org.gnome.desktop.input-sources xkb-options "['caps:escape', 'compose:rctrl']"
+
+  install_sf_fonts
+  set_gnome_option org.gnome.desktop.interface font-name 'SF Pro Display 11'
+  set_gnome_option org.gnome.desktop.interface monospace-font-name 'SF Mono 10'
+  set_gnome_option org.gnome.desktop.interface document-font-name 'SF Pro Text 11'
+
+  # TODO(andrei): relogin
+}
+
+components=('zsh' 'nvim' 'tmux' 'gnome')
 
 execute=false
 verbose=false
