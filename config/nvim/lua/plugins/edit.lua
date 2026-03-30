77e2bd3b18825a625ee030957e237487e33fc75f
Andrei Eremeev <aaeremeev@proton.me>
2026-03-30 12:41:53 +0300

    nvim: spider

--- config/nvim/lua/plugins/edit.lua
+++ config/nvim/lua/plugins/edit.lua
@@ -10,7 +10,16 @@ return {
   },
   {
     event = "VeryLazy",
-    "chaoren/vim-wordmotion",
+    "chrisgrieser/nvim-spider",
+    keys = {
+      { "w", "<cmd>lua require('spider').motion('w')<CR>", mode = { "n", "o", "x" } },
+      { "e", "<cmd>lua require('spider').motion('e')<CR>", mode = { "n", "o", "x" } },
+      { "b", "<cmd>lua require('spider').motion('b')<CR>", mode = { "n", "o", "x" } },
+      { "ge", "<cmd>lua require('spider').motion('ge')<CR>", mode = { "n", "o", "x" } },
+    },
+    opts = {
+      skipInsignificantPunctuation = false,
+    },
   },
   {
     'nvim-mini/mini.splitjoin',
